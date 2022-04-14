710386c088f974f52b02da1ee07007d100abc53d
Admin <sergeii.kirik@gmail.com>
2022-04-14 15:21:12 +0300

    modified gitignore

--- src/router/routes.js
+++ src/router/routes.js
@@ -5,7 +5,7 @@ import Friends from "../pages/Friends";
 import FriendInfo from "../components/FriendInfo";
 
 export const routes = [
-    {path: "/posts/home" , element: Home, exact: true},
+    {path: "/posts/" , element: Home, exact: true},
     {path: "/posts/posts" , element: Posts, exact: true},
     {path: "/posts/error" , element: Error, exact: true},
     {path: "/posts/friends" , element: Friends, exact: true},
@@ -13,7 +13,7 @@ export const routes = [
 ];
 
 export const links = [
-    {to: "/posts/home", text: "Home"},
+    {to: "/posts/", text: "Home"},
     {to: "/posts/posts", text: "Posts"},
     {to: "/posts/error", text: "Error"},
     {to: "/posts/friends", text: "Friends"},
